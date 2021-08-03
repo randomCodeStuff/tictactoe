73db6ddb9806c7f1e85bbb60cbbf8961e771b122
Sarah Ahmad <ahmad.sarah.333@gmail.com>
2021-08-02 21:03:53 -0400

    added redux to json added store quick save before anything goes wrong

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />,
+  </Provider>,
+  document.getElementById('root')
+);
