c1b2681db19f99b87be85bb3d98d065ee1674793
Sarah Ahmad <ahmad.sarah.333@gmail.com>
2021-08-08 17:46:06 -0400

    it did not crash anything, but its not working yet either

--- src/app/store.js
+++ src/app/store.js
@@ -1,39 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createReducer } from '@reduxjs/toolkit';
+import { createAction } from '@reduxjs/toolkit';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ReactReduxContext } from 'react-redux';
-//import {createStore} from 'redux';
 
-//type constraints
-const MAKEAIMOVE = 'MAKEAIMOVE';
-const ISXNEXT = 'ISXNEXT';
-const HISTORY = 'HISTORY';
+const xIsNext = createAction('xIsNext');
+const squares = createAction('squares');
+const history = createAction('history');
 
-//Actinon creater
-export const aimove = () => {
-  return {
-    type: MAKEAIMOVE,
-  };
-};
-
-export const player = () => {
-  return {
-    type: ISXNEXT,
-  };
-};
-
-export const viewhistory = () => {
-  return {
-    type: HISTORY,
-  };
-};
-
-//thunk to make move?
-export const aiMove(board) = aiMoveThunk(
-  async(board) =>{
-      const move=await bestAIMove(board);
-      return move.data;
-  }
-);
+// export const aiMove = aiMoveThunk(async (board) => {
+//   const move = await bestAIMove(board);
+//   return move.data;
+// });
 
 const initialState = {
   history: [
@@ -44,26 +21,24 @@ const initialState = {
   stepNumber: 0,
   xIsNext: true,
 };
+//thunk
+const aiMove = createAsyncThunk(initialState, {
+  //stuff
+});
 //reducer
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ISXNEXT:
-      return {
-        isXnext: {...state, xisNext: !xisNext} //might break this up
-      };
-    case HISTORY:
-      return {
-        history: {...state, history: 'not sure yet'}
-      };
-    case MAKEAIMOVE:
-      return {
-        board: {...state, board: 'not sure yet'}
-      };
-    default:
-      return state;
-  }
-};
+const reducer = createReducer(initialState, {
+  [history]: (state, action) => {
+    console.log(action);
+    return state;
+  },
+  [xIsNext]: (state, action) => {
+    console.log(action);
+    return state;
+  },
+  [squares]: (state, action) => {
+    console.log(action);
+    return state;
+  },
+});
 
-export default configureStore(reducer);
-
-//const store=createStore(rootReducer);
+export default configureStore({ reducer });
